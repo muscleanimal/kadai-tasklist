3957c6439d3a567df474e18fb426ce54892e4321
tatsuya.masuda <tm849908@gmail.com>
2019-08-24 03:16:01 +0000

    listing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,12 @@
 class TasksController < ApplicationController
       before_action :set_task, only: [:show, :edit, :update, :destroy]
+        before_action :require_user_logged_in
   def index
-    @tasks = Task.all.page(params[:page])
+        if logged_in?
+          @task = current_user.tasks.build
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    
+        end
   end
 
   def show
@@ -12,12 +17,15 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
+
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
     before_action :require_user_logged_in, only: [:index, :show]
   def index
-       @users = User.order(id: :desc).page(params[:page]).per(25)
+        @users = User.order(id: :desc).page(params[:page]).per(25)
+      @tasklist = current_user.tasks.build  # form_with 用
+      @tasklists = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
